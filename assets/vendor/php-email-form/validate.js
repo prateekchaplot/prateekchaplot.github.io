9abe7595038a31bd829a4be57c3181eda93637ad
Prateek Chaplot <prateekchaplot96@gmail.com>
2023-02-18 19:09:01 +0530

    add contact form

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -60,7 +60,7 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
+      if (!data.includes('Error')) {
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
